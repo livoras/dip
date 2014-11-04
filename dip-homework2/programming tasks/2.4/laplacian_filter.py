f123525b175867855957340aff7a03a50c8a57c3
livoras <livoras@163.com>
2014-11-04 16:53:23 +0800

    2 done

--- dip-homework2/programming tasks/2.4/laplacian_filter.py
+++ dip-homework2/programming tasks/2.4/laplacian_filter.py
@@ -8,14 +8,17 @@ def laplacian_filter(img):
 
     def makeLaplacian(x, y):
         total = 0
+        # 遍历某个元素附近的像素点，统计所有边缘像素的和
         for innerX in xrange(x - h_pixs, x + h_pixs + 1):
             for innerY in xrange(y - v_pixs, y + v_pixs + 1):
+                # 本元素不参与计算
                 if innerX == x and innerY == y: continue
                 try:
                     pix = img.getpixel((innerX, innerY))
                 except:
                     pix = 0
                 total += pix
+        # 使用书本上第二个Laplacian算子对像素值进行计算和写入
         newImg.putpixel((x, y), total - 8 * img.getpixel((x, y)))
 
     for x in xrange(width):
